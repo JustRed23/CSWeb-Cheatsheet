09ce34ea3b60307094f7efb7bdc6f02c91eecc32
JustRed23 <manuel.bormans@gmail.com>
2025-06-10 23:43:54 +0200

    Little blazor example

---
+++ Blazor/Controllers/BlazorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blazor.Controllers;
+
+public class BlazorController : Controller
+{
+    public IActionResult Index()
+    {
+        return View("_BlazorServer_Host");
+    }
+}
